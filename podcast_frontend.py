1ca851a7c0e6d3b210f2023b6ad311a8079866bd
YoungPhlo <dev@youngphlo.com>
2023-09-21 02:29:00 -0500

    move podcast summary above fold using placeholder.container()

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -9,6 +9,15 @@ import re
 def main():
     st.title("Podcast Dashboard")
 
+    # custom_header = st.empty()
+    # custom_subheader = st.empty()
+    # custom_title = st.empty()
+    # custom_columns = st.empty()
+    # custom_header_summary = st.empty()
+    # custom_summary = st.empty()
+    # custom_image = st.empty()
+    # custom_header_moments = st.empty()
+    # custom_moments = st.empty()
     custom_podcast = st.empty()
 
     available_podcast_info = create_dict_from_json_files('.')
@@ -104,43 +113,44 @@ def main():
     if process_button:
         # Call the function to process the URLs and retrieve podcast guest information
         podcast_info = process_podcast_info(url)
-    
-        # Right section - Newsletter content
-        custom_podcast.header("Newsletter Content")
-    
-        # Display the podcast title
-        custom_podcast.subheader("Episode Title")
-        custom_podcast.write(podcast_info['podcast_details']['episode_title'])
-    
-        # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = custom_podcast.columns([7, 3])
-    
-        with col1:
-            # Display the podcast episode summary
-            custom_podcast.subheader("Podcast Episode Summary")
-            custom_podcast.write(podcast_info['podcast_summary'])
-    
-        with col2:
-            custom_podcast.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300,
-                                 use_column_width=True)
-    
-        # # Display the podcast guest and their details in a side-by-side layout
-        # col3, col4 = st.columns([3, 7])
-    
-        # with col3:
-        #     st.subheader("Podcast Guest")
-        #     st.write(podcast_info['podcast_guest']['name'])
-    
-        # with col4:
-        #     st.subheader("Podcast Guest Details")
-        #     st.write(podcast_info["podcast_guest"]['summary'])
-    
-        # Display the five key moments
-        custom_podcast.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            custom_podcast.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+
+        with custom_podcast.container():
+            # Right section - Newsletter content
+            st.header("Newsletter Content")
+
+            # Display the podcast title
+            st.subheader("Episode Title")
+            st.write(podcast_info['podcast_details']['episode_title'])
+
+            # Display the podcast summary and the cover image in a side-by-side layout
+            col1, col2 = st.columns([7, 3])
+
+            with col1:
+                # Display the podcast episode summary
+                st.subheader("Podcast Episode Summary")
+                st.write(podcast_info['podcast_summary'])
+
+            with col2:
+                st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300,
+                         use_column_width=True)
+
+            # # Display the podcast guest and their details in a side-by-side layout
+            # col3, col4 = st.columns([3, 7])
+
+            # with col3:
+            #     st.subheader("Podcast Guest")
+            #     st.write(podcast_info['podcast_guest']['name'])
+
+            # with col4:
+            #     st.subheader("Podcast Guest Details")
+            #     st.write(podcast_info["podcast_guest"]['summary'])
+
+            # Display the five key moments
+            st.subheader("Key Moments")
+            key_moments = podcast_info['podcast_highlights']
+            for moment in key_moments.split('\n'):
+                st.markdown(
+                    f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
 
 
 def create_dict_from_json_files(folder_path):
